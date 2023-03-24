448ef43083a6a700dab770b92ed3ca723c6e3276
Hiroki Noda <kubo39@gmail.com>
2023-03-25 01:27:45 +0900

    fib: add runtime version for comparision

--- constant_expressions/fib.d
+++ constant_expressions/fib.d
@@ -1,10 +1,25 @@
 // https://nim-lang.github.io/Nim/manual.html#constants-and-constant-expressions
 //
 
-template fibonacci(int N) if (N > 0)
+template ctFibonacci(int N) if (N > 0)
 {
-    static if (N <= 2) enum fibonacci = 1;
-    else enum fibonacci = fibonacci!(N - 1) + fibonacci!(N - 2);
-    pragma(msg, fibonacci);
+    static if (N <= 2) enum ctFibonacci = 1;
+    else enum ctFibonacci = ctFibonacci!(N - 1) + ctFibonacci!(N - 2);
+    pragma(msg, ctFibonacci);
 }
-enum _ = fibonacci!(20);
+enum _ = ctFibonacci!(30);
+
+
+// runtime version.
+int rtFibonacci(int n)
+{
+    import std.stdio;
+    int ret = n <= 2 ? 1 : rtFibonacci(n - 1) + rtFibonacci(n - 2);
+    writeln(ret);  // called everytime!
+    return ret;
+}
+
+void main()
+{
+    rtFibonacci(30);
+}
